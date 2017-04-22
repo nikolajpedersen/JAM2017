49597613aa0af8336b3e0ee82e1384a6f5d6bdb4
nikolajpedersen <nikolaj@gameanalytics.com>
2017-04-22 20:13:45 +0200

    added a ghost + some rotation logic

---
+++ NGJ2017/Assets/RotateGhosts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGhosts : MonoBehaviour {
+
+	public GameObject dialObject;
+
+	// Use this for initialization
+	void Start () {
+		dialObject = GameObject.Find ("Dial");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion dialRotation = dialObject.transform.rotation;
+		transform.rotation = dialRotation;
+	}
+}
